cb46ff6c74ab08182f02759af1ba210a75974d98
Sushant Singh <sushantbibhu@gmail.com>
2024-03-05 18:00:25 +0530

    Add AddToCartButton to product page and create a getCart , createCart function

---
+++ src/lib/db/cart.ts
@@ -0,0 +1,50 @@
+import { cookies } from "next/dist/client/components/headers";
+import { prisma } from "./prisma";
+import { Cart, Prisma } from "@prisma/client";
+
+export type CartWithProducts = Prisma.CartGetPayload<{
+  include: { item: { include: { product: true } } };
+}>;
+
+export type ShoppingCart = CartWithProducts & {
+  size: number;
+  subtotal: number;
+};
+
+export async function getCart(): Promise<ShoppingCart | null> {
+  const localCartId = cookies().get("localCartId")?.value;
+  const cart = localCartId
+    ? await prisma.cart.findUnique({
+        where: { id: localCartId },
+        include: { item: { include: { product: true } } },
+      })
+    : null;
+
+  if (!cart) {
+    return null;
+  }
+
+  return {
+    ...cart,
+    size: cart.item.reduce((acc, item) => acc + item.quantity, 0),
+    subtotal: cart.item.reduce(
+      (acc, item) => acc + item.quantity * item.product.price,
+      0,
+    ),
+  };
+}
+
+export async function createCart(): Promise<ShoppingCart> {
+  const newCart = await prisma.cart.create({
+    data: {},
+  });
+
+  cookies().set("localCartId", newCart.id);
+
+  return {
+    ...newCart,
+    item: [],
+    size: 0,
+    subtotal: 0,
+  };
+}
